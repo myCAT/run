1488975d0868b827f16dbbcb8edce37e33ca2bda
myCAT <myCAT@olanto.org>
2013-02-28 10:27:17 +0100

    initial load

---
+++ conversion.bat
@@ -0,0 +1,2 @@
+java -Xmx512m -cp "C:\MYCAT\dist\SimpleConverter.jar" org.olanto.converter.SimpleConverterApplicationTest -f txt -b C:/MYCAT/corpus/bad C:/MYCAT/corpus/docs C:/MYCAT/corpus/source
+pause
